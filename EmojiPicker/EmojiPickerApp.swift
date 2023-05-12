dd0daccc36af4795306519ac1428bffa2c6b0ca8
devanshuchaudhary <52855516+devdchaudhary@users.noreply.github.com>
2023-05-12 10:31:29 +0530

    Initial Commit

---
+++ EmojiPicker/EmojiPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiPickerApp.swift
+//  EmojiPicker
+//
+//  Created by devdchaudhary on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
